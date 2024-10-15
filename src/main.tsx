7c6409558aaaa7749106f9916c748f9758b70891
Mikle25 <mikle2593@gmail.com>
2024-10-15 15:34:50 +0300

    change color mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ColorModeScript } from "@chakra-ui/react";
+import { theme } from "./theme/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </Provider>
   </React.StrictMode>
